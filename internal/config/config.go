b7840f2a7251eb1c76f978a5316c8854ea1fa992
Alexander Kalugin <103287576+al1ksn@users.noreply.github.com>
2026-04-19 17:23:50 +0500

    Created config and .env.example

---
+++ internal/config/config.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+    "fmt"
+    "os"
+    "github.com/joho/godotenv"
+)
+
+type Config struct {
+    DBHost     string
+    DBPort     string
+    DBUser     string
+    DBPassword string
+    DBName     string
+    ServerPort string
+}
+
+func Load() (*Config, error) {
+    _ = godotenv.Load()
+
+	cfg := &Config{
+		DBHost:     getEnv("DB_HOST", "localhost"),
+		DBPort:     getEnv("DB_PORT", "5432"),
+		DBUser:     getEnv("DB_USER", "postgres"),
+		DBPassword: getEnv("DB_PASSWORD", "postgres"),
+		DBName:     getEnv("DB_NAME", "subscriptions"),
+		ServerPort: getEnv("SERVER_PORT", "8080"),
+	}
+
+	return cfg, nil
+}
+
+func (c *Config) DSN() string {
+	return fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		c.DBUser, c.DBPassword, c.DBHost, c.DBPort, c.DBName,
+	)
+}
+
+func getEnv(key, fallback string) string {
+	if val := os.Getenv(key); val != "" {
+		return val
+	}
+	return fallback
+}
