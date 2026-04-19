c9bac0815e1058aacc30660d2a5234cdcfd7746e
Alexander Kalugin <103287576+al1ksn@users.noreply.github.com>
2026-04-19 18:06:03 +0500

    Add base for repository

---
+++ internal/repository/subscription.go
@@ -0,0 +1,13 @@
+package repository
+
+import (
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type SubscriptionRepository struct {
+    db *pgxpool.Pool
+}
+
+func NewSubscriptionRepository(db *pgxpool.Pool) *SubscriptionRepository {
+    return &SubscriptionRepository{db: db}
+}
